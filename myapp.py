0fefd6590bf8c388d2df6575178e83b21aeae906
Ryndine <rcmangeno@gmail.com>
2022-12-20 19:50:38 -0800

    'update'

--- myapp.py
+++ myapp.py
@@ -9,9 +9,8 @@ class DataFrameColumnAdder:
 
     def add_column(self, column_name, values):
         self.df[column_name] = values
-
-df = pd.read_csv("Resources/diabetes.csv")
-
+        
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
 column_adder = DataFrameColumnAdder(df)
 column_adder.add_column('C', [7, 8, 9])
 print(df)
